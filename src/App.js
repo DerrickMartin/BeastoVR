a2e4923d29873387e6f42c67353977910e2c3691
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-02-03 23:51:44 +0000

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.tehbeasto.cloud"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Teh Beasto
         </a>
       </header>
     </div>
