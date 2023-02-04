a00500d2855154cb1f075eb1a58c2b23cbf454ef
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-02-04 02:33:51 +0000

    Updated Header and Styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="App-Logo-Text"> てベ-スと</p>
-        <p>
-          Welcome, 
-          はじめまして
+        <a
+          className="App-Logo-Text"
+          href="https://www.tehbeasto.cloud"
+          target="_blank"
+          rel="noopener noreferrer">てベ-スと</a>
+        <p className="Nav-Routes"> 
+        Clothes
+        </p>
+        <p className="Nav-Routes"> 
+        Avatar
+        </p>
+        <p className="Nav-Routes"> 
+        World
         </p>
         <a
           className="App-link"
@@ -20,8 +29,38 @@ function App() {
           Teh Beasto
         </a>
       </header>
+      <body>
+        <p >
+          Welcome, 
+          はじめまして
+        </p></body>
     </div>
   );
 }
 
 export default App;
+
+
+
+/*      
+import logo from './logo.svg';
+
+Default image
+<img src={logo} className="App-logo" alt="logo" />
+
+Css styling (default)
+@media (prefers-reduced-motion: no-preference) {
+  .App-logo {
+    animation: App-logo-spin infinite 20s linear;
+  }
+}
+@keyframes App-logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+*/
