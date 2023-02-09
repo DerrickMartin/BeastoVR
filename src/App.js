ce687f1ea9e3fa82a5a4eb4241f6469c40000d6d
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-02-08 19:23:37 -0800

    Update App.js removing comments

--- src/App.js
+++ src/App.js
@@ -49,28 +49,3 @@ function App() {
 }
 
 export default App;
-
-
-
-/*      
-import logo from './logo.svg';
-
-Default image
-<img src={logo} className="App-logo" alt="logo" />
-
-Css styling (default)
-@media (prefers-reduced-motion: no-preference) {
-  .App-logo {
-    animation: App-logo-spin infinite 20s linear;
-  }
-}
-@keyframes App-logo-spin {
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-}
-
-*/
