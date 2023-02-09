bc915266f68e17e8b2ad63310d9ee634e7846c58
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-02-08 20:42:07 -0800

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,14 +6,12 @@ function Navbar() {
         return (
         <header className='app-header'>
             <div className='Logo'>
-                <h1>
-                    <a
-                        className="App-Logo-Text"
-                        href="https://www.tehbeasto.cloud"
-                        target="_blank"
-                        rel="noopener noreferrer">てベ-スと
-                    </a>
-                </h1>
+                <a
+                  className="App-Logo-Text"
+                  href="https://www.tehbeasto.cloud"
+                  target="_blank"
+                  rel="noopener noreferrer">てベ-スと
+                </a>
             </div>
             <div className='header-links'>
                 <ul className="web-nav-links">
