15499479ee3957e324f5f16a3f4aed787f90d240
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-02-09 17:51:57 +0000

    Fixed Logo name

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,7 +13,7 @@ function Navbar() {
                         className="App-Logo-Text"
                         href="https://www.tehbeasto.cloud"
                         target="_blank"
-                        rel="noopener noreferrer">てベ-スと
+                        rel="noopener noreferrer">テベ-スと
                     </a>
                 </h1>
             </div>
