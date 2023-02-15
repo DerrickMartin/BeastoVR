b911f9014a58143442a131db21345ffd20f46d49
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-02-15 00:03:26 +0000

    Updated Footer with image and social media links

--- src/components/Footer/FooterSection.js
+++ src/components/Footer/FooterSection.js
@@ -2,22 +2,29 @@ import { React } from "react";
 import './FooterStyles.css';
 
 import '../../styles/globalstyles.css';
+import FooterLogo from '../../assets/Logo.png';
 import twitch from '../../assets/pictures/Twitchpng.png';
 
 function FooterSection() {
 
         return (
         <footer className='App-Footer'>
-          <p className="App-Logo-Text">テビ -スと</p>
-          <p>Copyright © 2023 TehBeasto Inc. All rights reserved.</p>
+           <img src={FooterLogo} className="LogoIcon" alt="TehBeasto Logo" />
             <div className="bottom-links">
                 <a href="www.tehbeasto.cloud">Home</a> 
                 <a href="www.tehbeasto.cloud">Contact Us </a>
                 <a href="www.tehbeasto.cloud">Worlds</a>
                 <a href="www.tehbeasto.cloud">Clothes</a>
                 <a href="www.tehbeasto.cloud">Social Media Icons</a>
-                <img src={twitch} className="social-media" alt="TehBeasto's Twitch" />
-
+                
+            </div>
+            <div className="copywriteSocialLinks">
+            <p>Copyright © 2023 TehBeasto Inc. All rights reserved.</p>
+              <div className="social-links">
+                <img src={twitch} className="social-media" alt="TehBeasto's Twitch" />   
+                <img src={twitch} className="social-media" alt="TehBeasto's Twitch" />  
+                <img src={twitch} className="social-media" alt="TehBeasto's Twitch" /> 
+              </div>          
             </div>
         </footer>
         )
