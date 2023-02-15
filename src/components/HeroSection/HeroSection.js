c41be1366b4acd5815688555743f31e953c2b7c1
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-02-14 19:55:32 -0800

    Created HeroSection component

---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,14 @@
+import { React } from "react";
+import './HeroSectionStyles.css';
+
+import '../../styles/globalstyles.css';
+
+
+function HeroSection() {
+
+        return (
+       <p>Hero Section</p>
+        )
+
+}
+ export default HeroSection
