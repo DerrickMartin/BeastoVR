aebbdf3caa6e40813ca309de026f21e08d4f8012
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-02-16 19:07:53 +0000

    Updated menu positioning

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,14 +11,12 @@ function Navbar() {
         return (
         <header className='app-header'>
             <div className='Logo'>
-                <h1>
                     <a
                         href="https://www.tehbeasto.cloud"
                         target="_blank"
                         rel="noopener noreferrer">
                             <img src={NavLogo} className="LogoIcon" alt="TehBeasto Logo" />
                     </a>
-                </h1>
             </div>
             <div className='header-links'>
                 <Hamburger toggled={isOpen} toggle={setOpen} />
