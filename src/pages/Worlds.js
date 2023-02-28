df51e0f669b5ca2d65a7be19f08cbaf40adc84ee
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-02-28 05:21:18 +0000

    Added right hand and grab ability for vr

--- src/pages/Worlds.js
+++ src/pages/Worlds.js
@@ -4,16 +4,19 @@ import React, { Component } from 'react';
 class Worlds extends Component {
   render () {
     return (
-      <a-scene>
+      <a-scene id="rightHand" hand-controls="hand: right; handModelStyle: highPoly; color: #ffcccc">
         <a-box 
+      grabbable
       position="-1 0.5 -3" 
       rotation="0 45 0" 
       color="#4CC3D9" />
     <a-sphere 
+      grabbable
       position="0 1.25 -5" 
       radius="1.25" 
       color="#EF2D5E" />
     <a-cylinder 
+      grabbable
       position="1 0.75 -3" 
       radius="0.5" 
       height="1.5" 
@@ -25,6 +28,7 @@ class Worlds extends Component {
       height="4" 
       color="#7BC8A4" />
     <a-dodecahedron 
+      grabbable
       color="#FF926B" 
       radius="5" 
       position="0 -1 -30"></a-dodecahedron>
