9b7fae85a29b9abb930f83c9092bc3d8381230b0
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-03-01 03:32:17 +0000

    Cleaned up code for Worlds.js

--- src/pages/Worlds.js
+++ src/pages/Worlds.js
@@ -8,34 +8,38 @@ class Worlds extends Component {
     return (
       <div className='world1'>
       <a-scene  sphere-collider="objects: a-box;" super-hands id="rightHand" hand-controls="hand: right; handModelStyle: lowPoly; color: #ffcccc">
+        <a-assets>
+          <img id="combatFlooring" src="https://imgur.com/a/1SxpGIO" alt="Plane material" />
+        </a-assets>
         <a-box 
-      grabbable
-      position="-1 0.5 -3" 
-      rotation="0 45 0" 
-      color="#4CC3D9" />
-    <a-sphere 
-      grabbable
-      position="0 1.25 -5" 
-      radius="1.25" 
-      color="#EF2D5E" />
-    <a-cylinder 
-      grabbable
-      position="1 0.75 -3" 
-      radius="0.5" 
-      height="1.5" 
-      color="#FFC65D" />
-    <a-plane 
-      position="0 0 -4" 
-      rotation="-90 0 0" 
-      width="25" 
-      height="25" 
-      src="https://imgur.com/a/1SxpGIO" />
-    <a-dodecahedron 
-      grabbable
-      color="#FF926B" 
-      radius="5" 
-      position="0 -1 -30"></a-dodecahedron>
-    <a-sky src={require('../assets/skybox/cloudyNight.jpg')} /></a-scene>
+          grabbable
+          position="-1 0.5 -3" 
+          rotation="0 45 0" 
+          color="#4CC3D9" />
+        <a-sphere 
+          grabbable
+          position="0 1.25 -5" 
+          radius="1.25" 
+          color="#EF2D5E" />
+        <a-cylinder 
+          grabbable
+          position="1 0.75 -3" 
+          radius="0.5" 
+          height="1.5" 
+          color="#FFC65D" />
+        <a-plane 
+          position="0 0 -4" 
+          rotation="-90 0 0" 
+          width="25" 
+          height="25" 
+          src="#combatFlooring" />
+        <a-dodecahedron 
+          grabbable
+          color="#FF926B" 
+          radius="5" 
+          position="0 -1 -30" />
+        <a-sky src={require('../assets/skybox/cloudyNight.jpg')} />
+      </a-scene>
       </div>
     );
   }
