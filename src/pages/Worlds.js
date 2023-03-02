e307832352994f2e92c13ef7110e9e22349f6fcc
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-03-02 17:21:53 +0000

    Updated worlds js

--- src/pages/Worlds.js
+++ src/pages/Worlds.js
@@ -1,18 +1,10 @@
-import 'aframe';
-import 'aframe-animation-component';
-import 'aframe-particle-system-component';
-import 'babel-polyfill';
-import {Entity, Scene} from 'aframe-react';
-import React from 'react';
-import ReactDOM from 'react-dom';
+
 import "./WorldsStyles.css";
-class Worlds extends React.Component {
-  render () {
+const Worlds = () => {
     return (
       <div id="sceneContainer">
         hey
       </div>
     );
-}
 }
 export default Worlds;
