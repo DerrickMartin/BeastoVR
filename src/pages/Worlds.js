95a257acdedf3c8210da41c0dad59b4c3dcd06f9
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-03-02 16:49:40 +0000

    Updated worlds page

--- src/pages/Worlds.js
+++ src/pages/Worlds.js
@@ -1,49 +1,59 @@
 import 'aframe';
-import React, { Component } from 'react';
-
+import 'aframe-animation-component';
+import 'aframe-particle-system-component';
+import 'babel-polyfill';
+import {Entity, Scene} from 'aframe-react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import "./WorldsStyles.css";
-class Worlds extends Component {
+class Worlds extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {color: 'red'};
+  }
+
+  changeColor() {
+    const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
+    this.setState({
+      color: colors[Math.floor(Math.random() * colors.length)]
+    });
+  }
   render () {
     return (
-      <div className='world1'>
-        <a-scene  sphere-collider="objects: a-box;" super-hands id="rightHand" hand-controls="hand: right; handModelStyle: lowPoly; color: #ffcccc">
-          <a-assets>
-            <img id="combatFlooring" src="../assets/grass_texture.jpeg" alt="Plane material" />
-            <a-asset-item id="chuuninRoom" src="../assets/gltf_modals/chunnin_scene.gltf"></a-asset-item>
-          </a-assets>
-          <a-entity camera="active: true" look-controls wasd-controls position="0 1.6 0" data-aframe-default-camera></a-entity>
-          <a-gltf-model src="#chuuninRoom"></a-gltf-model>
-          <a-box 
-            grabbable
-            position="-1 0.5 -3" 
-            rotation="0 45 0" 
-            color="#4CC3D9" />
-          <a-sphere 
-            grabbable
-            position="0 1.25 -5" 
-            radius="1.25" 
-            color="#EF2D5E" />
-          <a-cylinder 
-            grabbable
-            position="1 0.75 -3" 
-            radius="0.5" 
-            height="1.5" 
-            color="#FFC65D" />
-          <a-plane 
-            position="0 0 0" 
-            rotation="-90 0 0" 
-            width="55" 
-            height="55" 
-            src="#combatFlooring" />
-          <a-dodecahedron 
-            grabbable
-            color="#FF926B" 
-            radius="5" 
-            position="0 -1 -30" />
-          <a-sky src={require('../assets/skybox/cloudyNight.jpg')} />
-        </a-scene>
+      <div id="sceneContainer">
+      <Scene>
+        <a-assets>
+          <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg" alt='groundTexture'/>
+          <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg" alt='skytexture'/>
+        </a-assets>
+
+        <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
+        <Entity primitive="a-light" type="ambient" color="#445451"/>
+        <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
+        <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
+        <Entity particle-system={{preset: 'snow', particleCount: 2000}}/>
+        <Entity text={{value: 'Hello, A-Frame React!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
+
+        <Entity id="box"
+          geometry={{primitive: 'box'}}
+          material={{color: this.state.color, opacity: 0.6}}
+          animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
+          animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
+          position={{x: 0, y: 1, z: -3}}
+          events={{click: this.changeColor.bind(this)}}>
+          <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
+                  geometry={{primitive: 'box', depth: 0.2, height: 0.2, width: 0.2}}
+                  material={{color: '#24CAFF'}}/>
+        </Entity>
+
+        <Entity primitive="a-camera">
+          <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
+        </Entity>
+      </Scene>
+
       </div>
     );
   }
 }
 export default Worlds;
+ReactDOM.render(<Worlds/>, document.querySelector('#sceneContainer'));
