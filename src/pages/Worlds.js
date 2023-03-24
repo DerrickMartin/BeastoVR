fc831d244b74b722efeb32b4a4db16d6d2336108
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-03-24 20:15:46 +0000

    Updated world page content

--- src/pages/Worlds.js
+++ src/pages/Worlds.js
@@ -6,7 +6,23 @@ const Worlds = () => {
       <>
       <Navbar />
       <div id="sceneContainer">
-        hey
+        <div className='WorldHero'>
+        <p>Glad you stopped by! Here are some of my favorite sceneries I created so we can explore in VR!!</p>
+        
+        <p>Choose any of the following worlds below, more worlds will be created in the future, with the voted suggestions 
+          coming from my patreons </p>
+
+        <p> Patreon Button</p>
+
+        <ul> 
+          <li>World 1</li>
+          <li>World 2</li>
+          <li>World 3</li>
+        </ul>
+
+
+
+        </div>
       </div>
       </>
     );
