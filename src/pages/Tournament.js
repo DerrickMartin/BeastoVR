583fc193332d8818a3a4ba3168035a39833dc97e
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-03-29 02:28:47 +0000

    Updated Tournament Page

--- src/pages/Tournament.js
+++ src/pages/Tournament.js
@@ -11,15 +11,15 @@ const Tournament = () => {
         <h3>🏆 Fight for Glory, Honor, and Respect! 🏆</h3>
 
         <div style={{padding: 5}}>
-          <p>Im excited to preset to you the location to join where tons of battles for glory and power will be broadcasted! </p>
+          <p>I am eager to preset to you the location to join where tons of battles for glory, fame, and dominace will be broadcasted! </p>
 
           <p>Here you will be able to join the game world and enhance your immersion when the tournaments commece.</p>
 
-          <p>With 2 of the most well known animes releasing sequels to games that have long thought to be in retirement!</p>
+          <p>With the release of successors to 2 of the most well known animes releasing sequels to games that have long thought to be in retirement! </p>
 
-          <p>We will be hosting tournaments here where the Victors can be Honored for their talent and skills along with recognition.</p>
+          <p>We will be hosting tournaments here, where the victors will be honored for their talent and skills. </p>
 
-          <p>The Two games will be having Monthly Tournaments I hope to see you here with us after their release dates in 2023.</p>
+          <p>After the two games release in 2023, I hope to see you here with us for our monthly tournaments.</p>
         </div>
       </div>
       <FooterSection />
