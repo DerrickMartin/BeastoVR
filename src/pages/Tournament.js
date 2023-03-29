4d3d7c3f1e460c0ab2297955a46605f3fc1d0824
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-03-29 01:17:16 +0000

    Added styling to Tournament Page

--- src/pages/Tournament.js
+++ src/pages/Tournament.js
@@ -8,16 +8,19 @@ const Tournament = () => {
       <>
       <Navbar />
       <div id="SceneContainer">
-        <h2>🏆 Fight for Glory, Honor, and Respect! 🏆</h2>
-        <p>Im excited to preset to you the location to join where tons of battles for glory and power will be broadcasted! </p>
+        <h3>🏆 Fight for Glory, Honor, and Respect! 🏆</h3>
 
-        <p>Here you will be able to join the game world and enhance your immersion when the tournaments commece.</p>
+        <div style={{padding: 5}}>
+          <p>Im excited to preset to you the location to join where tons of battles for glory and power will be broadcasted! </p>
 
-        <p>With 2 of the most well known animes releasing sequels to games that have long thought to be in retirement!</p>
+          <p>Here you will be able to join the game world and enhance your immersion when the tournaments commece.</p>
 
-        <p>We will be hosting tournaments here where the Victors can be Honored for their talent and skills along with recognition.</p>
+          <p>With 2 of the most well known animes releasing sequels to games that have long thought to be in retirement!</p>
 
-       <p>The Two games will be having Monthly Tournaments I hope to see you here with us after their release dates in 2023.</p>
+          <p>We will be hosting tournaments here where the Victors can be Honored for their talent and skills along with recognition.</p>
+
+          <p>The Two games will be having Monthly Tournaments I hope to see you here with us after their release dates in 2023.</p>
+        </div>
       </div>
       <FooterSection />
       </>
