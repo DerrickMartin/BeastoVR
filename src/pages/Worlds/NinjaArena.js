897c6211a6521a0bfe3630cf333c20487c9a8706
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-03-30 20:17:40 +0000

    Updated Ninja Arena

--- src/pages/Worlds/NinjaArena.js
+++ src/pages/Worlds/NinjaArena.js
@@ -21,22 +21,22 @@ const NinjaArena = () => {
         crossorigin="anonymous" 
         alt="chuunin Floor" />
       </a-assets>
-        <Entity primitive="a-plane" rotation="-90 0 0" height="60" width="60" src="#chuunin_floor"/>
+        <Entity primitive="a-plane" rotation="-90 0 0" height="120" width="120" src="#chuunin_floor"/>
         <Entity primitive="a-plane" rotation="0 0 0" height="4" width="4" src="#chuunin_floor" position="0, 2 1"/>
         <Entity primitive="a-light" type="ambient" color="#abbfbb" />
         <Entity primitive="a-light" type="point" intensity="1.5" position="2 4 4"/>
-        <Entity text={{value: 'Welcome to Ninja Arena!', align: "center", color: "#FFC650", width:"2.5", height:"2.5"}} position={{x: 0, y:3.5, z: -1}}/>
+        <Entity text={{value: 'Welcome to Ninja Arena!', align: "center", color: "#FFC650", width:"2.5", height:"2.5"}} position={{x: 0, y:2.5, z: -1}}/>
 
         <Entity id="plank"
           geometry={{primitive: 'box', width: 22, height:0.3, depth: 1}}
           position={{x: -14, y: 10.8, z: -3.5}}
           material={{color:"#804000"}}/>
-        <Entity id="cylinderStart"
-          geometry={{primitive: 'cylinder', radius:5, height: 20}}
-          position={{x:1, y: 0.75, z: -3}}
+        <Entity id="cylinderright"
+          geometry={{primitive: 'cylinder', radius:2, height: 16}}
+          position={{x:30, y: 0.75, z: -3}}
           material={{color: "#FFC65D"}}/>
-        <Entity id="cylinderStart"
-          geometry={{primitive: 'cylinder', radius:5, height: 20}}
+        <Entity id="cylinderleft"
+          geometry={{primitive: 'cylinder', radius:2, height: 16}}
           position={{x:-30, y: 0.75, z: -3}}
           material={{color: "#FFC65D"}}/>
         <Entity primitive="a-camera" position={{x:0, y: 3, z: -1}} />
