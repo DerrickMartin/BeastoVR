45d7b0af4b279469bfc41bc149517fa3aff9df6b
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-03-30 21:51:56 +0000

    Added a wall to Ninja Arena

--- src/pages/Worlds/NinjaArena.js
+++ src/pages/Worlds/NinjaArena.js
@@ -22,11 +22,16 @@ const NinjaArena = () => {
         alt="chuunin Floor" />
       </a-assets>
         <Entity primitive="a-plane" rotation="-90 0 0" height="120" width="120" src="#chuunin_floor"/>
-        <Entity primitive="a-plane" rotation="0 0 0" height=".8" width="1.2" color="#000" position="0, 2.5 -2.3"/>
+        <Entity primitive="a-plane" rotation="0 0 0" height=".8" width="1.3" color="#000" position="0, 2.6 -2.3"/>
         <Entity primitive="a-light" type="ambient" color="#abbfbb" />
         <Entity primitive="a-light" type="point" intensity="1.5" position="2 4 4"/>
         <Entity text={{value: 'Welcome to Ninja Arena!', align: "center", color: "#FFC650", width:"2.5", height:"2.5"}} position={{x: 0, y:2.7, z: -2.29}}/>
         <Entity text={{value: 'Jutsus coming soon!', align: "center", color: "#FFC650", width:"2.5", height:"2.5"}} position={{x: 0, y:2.5, z: -2.29}}/>
+        <Entity id="leftWall"
+          geometry={{primitive: 'plane', width: 140, height:16, depth: 1}}
+          position={{x: -14, y: 0, z: 0}}
+          rotation={{x:90, y:0, z:0}}
+          material={{color:"#804000"}}/>
         <Entity id="plank"
           geometry={{primitive: 'box', width: 22, height:0.3, depth: 1}}
           position={{x: -14, y: 10.8, z: -3.5}}
