7b5fce80ecd68f790dbed033ab23ac3efd88d73a
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-03-30 22:52:19 +0000

    Updated Ninja Arena with another platform

--- src/pages/Worlds/NinjaArena.js
+++ src/pages/Worlds/NinjaArena.js
@@ -21,35 +21,36 @@ const NinjaArena = () => {
         alt="chuunin Floor" />
       </a-assets>
         <Entity id="floor" primitive="a-plane" rotation="-90 0 0" height="120" width="120" src="#chuunin_floor"/>
-        <Entity id="ceiling" primitive="a-plane" rotation="-90 0 0" height="120" width="120" src="#chuunin_floor" position="0, 16, 0"/>
+        <Entity id="ceiling" primitive="a-plane" rotation="90 0 0" height="120" width="120" src="#chuunin_floor" position="0, 16, 0"/>
         <Entity primitive="a-plane" rotation="0 0 0" height=".8" width="1.3" color="#000" position="0, 2.6 -2.3"/>
         <Entity primitive="a-light" type="ambient" color="#abbfbb" />
         <Entity primitive="a-light" type="point" intensity="1.5" position="2 4 4"/>
         <Entity text={{value: 'Welcome to Ninja Arena!', align: "center", color: "#FFC650", width:"2.5", height:"2.5"}} position={{x: 0, y:2.7, z: -2.29}}/>
         <Entity text={{value: 'Jutsus coming soon!', align: "center", color: "#FFC650", width:"2.5", height:"2.5"}} position={{x: 0, y:2.5, z: -2.29}}/>
         <Entity id="leftWall"
-          geometry={{primitive: 'box', width: 120, height:16, depth: 1}}
-          position={{x: -60, y: 5, z: 0}}
+          geometry={{primitive: 'box', width: 120, height:25, depth: 1}}
+          position={{x: -60, y: 8, z: 0}}
           rotation={{x:0, y:90, z:0}}
           material={{color:"#804000"}}/>
         <Entity id="rightWall"
-          geometry={{primitive: 'box', width: 120, height:16, depth: 1}}
-          position={{x:60, y:5, z:0}}
+          geometry={{primitive: 'box', width: 120, height:25, depth: 1}}
+          position={{x:60, y:8, z:0}}
           rotation={{x:0, y:90, z:0}}
           material={{color:"#804000"}}/>
         <Entity id="backWall"
-          geometry={{primitive: 'box', width: 120, height:16, depth: 1}}
-          position={{x:0, y: 5, z:60}}
+          geometry={{primitive: 'box', width: 120, height:25, depth: 1}}
+          position={{x:0, y: 8, z:60}}
           rotation={{x:0, y:0, z:0}}
           material={{color:"#804000"}}/>
         <Entity id="FrontWall"
-          geometry={{primitive: 'box', width: 120, height:16, depth: 1}}
-          position={{x:0, y: 5, z:-60}}
+          geometry={{primitive: 'box', width: 120, height:25, depth: 1}}
+          position={{x:0, y: 8, z:-60}}
           rotation={{x:0, y:0, z:0}}
           material={{color:"#804000"}}/>
-        <Entity id="plank"
-          geometry={{primitive: 'box', width: 22, height:0.3, depth: 1}}
-          position={{x: -14, y: 10.8, z: -3.5}}
+        <Entity id="leftPlatform"
+          geometry={{primitive: 'box', width: 110, height:0.3, depth: 2}}
+          position={{x: -58, y: 10.8, z: -3.5}}
+          rotation={{x:0, y: 0, z:90}}
           material={{color:"#804000"}}/>
         <Entity id="cylinderright"
           geometry={{primitive: 'cylinder', radius:1, height: 16}}
