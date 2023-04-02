3f8f2cb850f63d0461daf9ff4a200ae9cf074fd1
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-04-02 08:10:13 +0000

    Updated scale of model

--- src/pages/Worlds/NinjaArena.js
+++ src/pages/Worlds/NinjaArena.js
@@ -42,7 +42,7 @@ const NinjaArena = () => {
         <a-asset-item id="arena" src="src/pages/Worlds/ninja_arena.glb"></a-asset-item>
       </a-assets>
         <a-entity gltf-model="#arena"
-        position={{x: 0, y:0.5, z: 0}}></a-entity>
+        position="0, 0.5 0" scale="0.18, 0.18, 0.18"></a-entity>
 
         <Entity id="floor" primitive="a-plane" rotation="-90 0 0" height="120" width="120" src="#chuunin_floor"/>
         <Entity id="ceiling" primitive="a-plane" rotation="90 0 0" height="120" width="120" src="#chuunin_floor" position="0, 35, 0"/>
@@ -50,7 +50,7 @@ const NinjaArena = () => {
         <Entity primitive="a-light" type="ambient" color="#abbfbb" />
         <Entity primitive="a-light" type="point" intensity="1.5" position="2 4 4"/>
         <Entity text={{value: 'Welcome to Ninja Arena!', align: "center", color: "#FFC650", width:"2.5", height:"2.5"}} position={{x: 0, y:2.7, z: -2.29}}/>
-        <Entity text={{value: 'Elemental skills coming soon!', align: "center", color: "#FFC650", width:"2.5", height:"2.5"}} position={{x: 0, y:2.5, z: -2.29}}/>
+        <Entity text={{value: 'Skills coming soon!', align: "center", color: "#FFC650", width:"2.5", height:"2.5"}} position={{x: 0, y:2.5, z: -2.29}}/>
         <Entity id="leftWall"
           geometry={{primitive: 'box', width: 120, height:45, depth: 1}}
           position={{x: -60, y: 13, z: 0}}
